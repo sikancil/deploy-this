17625ed9e0b5fa75c85500a30c2a7d7462abfb81
D4W <dimas.arif.widianto@gmail.com>
2024-11-11 23:11:20 +0800

    📦 build(jest): update jest configuration and add tests
    - Adjusted test roots and test match patterns for better organization
    - Added module file extensions and coverage directory configuration
    - Introduced a new test file with a simple truthy check to verify setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  testMatch: ['**/test/**/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // roots: ["<rootDir>/src", "<rootDir>/test"],
+  roots: ["test"],
+  testMatch: ["**/?(*.)+(spec|test).ts"],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    "^.+\\.(t|j)s$": "ts-jest",
   },
-};
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  // collectCoverageFrom: ["**/test/**/*.(t|j)s"],
+  coveragePathIgnorePatterns: ["./node_modules/", "./dist/", "./coverage/"],
+  coverageDirectory: "./coverage",
+}
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe("index", () => {
+  it("return true", () => {
+    expect(() => true).toBeTruthy()
+  })
+})
